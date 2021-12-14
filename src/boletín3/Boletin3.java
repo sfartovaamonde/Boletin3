41facaf013977584127a172ca6fc7d5b826a071e
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2021-12-14 10:14:51 +0100

    Programación

---
+++ src/boletín3/Boletin3.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletín3;
+import java.util.Scanner;
+/**
+ *
+ * @author dam1
+ */
+public class Boletin3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    /*float lado=3f;
+    /*
+    float area=lado*lado;
+    System.out.println("o area de cadrado é="+area);*/
+   
+    /*float area=(float)Math.pow(3, 2);   
+    System.out.println("o area do cadrado é ="+area);*/
+   
+    float euros;
+    /*String frase;*/
+    
+    /*Scanner sc=new Scanner (System.in);
+    System.out.println(" teclea euros: ");
+    
+    euros=sc.nextFloat();
+    
+    System.out.println(" euros =" +euros);    
+    
+    float euroADolar;
+    System.out.println(" teclea euroADolar: ");
+    euroADolar=sc.nextFloat();
+    System.out.println(" euroADolar="+euroADolar);
+    
+    float dolares=euros*euroADolar;
+    System.out.println(euros+ "euros equivalen a "+ dolares+ "dolares" );*/
+   
+    /*int num1;
+    String frase;
+    
+    Scanner sc=new Scanner (System.in);
+    System.out.println(" teclea o num1: ");
+    
+    num1=sc.nextInt();
+    
+    System.out.println("num1="+num1);
+    
+    int num2;
+    System.out.println(" teclea o num2: ");
+   
+    num2=sc.nextInt();
+    
+    System.out.println("num2="+num2);
+    
+    int suma=num1+num2;
+    int resta=num1-num2;
+    int resta2=num2-num1;
+    int producto=num1*num2;
+    int cociente=num1/num2;
+    int cociente2=num2/num1;
+    
+    System.out.println("a suma="+suma);
+    System.out.println(" a resta="+resta);
+    System.out.println(" a resta2="+resta2);
+    System.out.println(" o producto="+producto);
+    System.out.println(" o cociente="+cociente);
+    System.out.println(" o cociente2="+cociente2);*/
+    
+    
+    
+    float preciotarifa, preciopagado;
+    Scanner sc=new Scanner (System.in);
+    System.out.println(" cuanto es el precio tarifa: ");
+    
+    preciotarifa=sc.nextFloat();
+    System.out.println("precio tarifa="+preciotarifa);
+  System.out.println(" cuanto es el precio pagado: ");
+    preciopagado=sc.nextFloat();
+    
+    System.out.println(" precio pagado="+ preciopagado);
+    
+    float descuento=((preciotarifa-preciopagado)/100)*preciotarifa;
+    
+    System.out.println(" visualizar descuento= "+descuento);
+    
+    
+    
+    
+    
+   
+    
+    
+    
+   
+    
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
